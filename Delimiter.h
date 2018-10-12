71ffac584b0e9452051fb53e534d016128ff3782
Chris <galdi@chapman.edu>
2018-10-12 05:09:17 +0000

    Adding Delimiter.h

---
+++ Delimiter.h
@@ -0,0 +1,20 @@
+/*
+Chris Galdi
+2299616
+galdi@chapman.edu
+CPSC 350-01
+Assignment 3
+*/
+
+#ifndef DELIMITER_H
+#define DELIMITER_H
+
+class Delimiter
+{
+public:
+  static char flip(char del);
+};
+
+
+
+#endif
